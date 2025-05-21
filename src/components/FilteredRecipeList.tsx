6ab5f18236199527a62d9b97791d539add648568
Hyuntak Kim <kimhyungtak@gmail.com>
2025-05-22 07:49:16 +0900

    처음 저장_검색까지 완료

---
+++ src/components/FilteredRecipeList.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { useState } from 'react'
+import { supabase } from '@/lib/supabaseClient'
+import IngredientSearchForm from '@/components/IngredientSearchForm'
+
+type Recipe = {
+  id: number
+  name: string
+}
+
+export default function FilteredRecipeList() {
+	const [recipes, setRecipes] = useState<Recipe[]>([])
+	const [loading, setLoading] = useState(false)
+
+	const searchRecipes = async (ingredients: string[]) => {
+		if (ingredients.length === 0) {
+			setRecipes([])
+		return
+		}
+
+		setLoading(true)
+		const { data, error } = await supabase
+			.schema('recipes')
+			.rpc('search_recipes_by_ingredients', {
+			ingredients_param: ingredients
+			})
+			.select();
+
+			console.log('RPC data:', data);
+			console.log('RPC error:', error);
+		if (error) {
+			console.error('검색 오류:', error)
+			setRecipes([])
+		} else {
+			setRecipes(data ?? [])
+		}
+		setLoading(false)
+ }
+
+	return (
+		<div className="my-6">
+			<h2 className="text-xl font-semibold mb-2">재료로 요리 검색하기</h2>
+			<IngredientSearchForm onSearch={searchRecipes} />
+			{loading ? (
+				<p>검색 중...</p>
+			) : (
+				<ul className="list-disc pl-6">
+					{recipes.map(recipe => (
+					<li key={recipe.id}>{recipe.name}</li>
+					))}
+				</ul>
+			)}
+		</div>
+ )
+}
---
+++ src/components/IngredientSearchForm.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useState } from 'react'
+
+type IngredientSearchFormProps = {
+    onSearch: (ingredients: string[]) => void
+}
+
+export default function IngredientSearchForm({ onSearch }: IngredientSearchFormProps) {
+  const [input, setInput] = useState('')
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault()
+	
+		const ingredients = input.split(',')
+			.map(item => item.trim())
+			.filter(item => item.length > 0)
+		onSearch(ingredients)
+	}
+
+	return (
+		<form onSubmit={handleSubmit} className="flex gap-2 mb-4">
+			<input
+				type="text"
+				value={input}
+				onChange={(e) => setInput(e.target.value)}
+				placeholder="재료를 입력 해주세요. (예: 소금, 돼지고기"
+				className="border px-2 flex-1"
+			/>
+			<button type="submit" className="bg-green-500 text-white px-4 py-2 rounded">
+				검색
+			</button>
+		</form>
+	)
+}
